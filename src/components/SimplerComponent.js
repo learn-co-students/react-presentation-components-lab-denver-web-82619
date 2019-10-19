7b2a74d43ace91fcf81d40556752ce2598e78457
colinhalexander <chalexander@icloud.com>
2019-10-19 13:52:11 -0600

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,16 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+const SimplerComponent = () => {
+  
+  const handleClick = (event) => {
+    const [r, g, b] = [r, g, b].map(() => Math.random()*255 )
+    event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+  }
+
+  return (
+    <div onClick={handleClick}>I am just happy</div>
+  )
+}
+
+export default SimplerComponent
